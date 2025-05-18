64cf95f3df94526599295c416d542502092bc364
salsavor <salvadorviegas693@gmail.com>
2025-05-18 15:00:30 +0100

    head

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/navbar/index.jsx';
+import Header from './components/header/index.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
+      <Header />
       <Navbar />
       <App />
     </Router>
