b693439db02923a6b080117ebd8067fe3f1d2291
Gaah <gabriellopes3d@gmail.com>
2025-05-18 16:49:20 +0100

    deus é bom e o diabo não presta
    amém

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
